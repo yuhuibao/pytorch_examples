d7de31109ab9e5be73ffcfe5cca84c974c89ec90
Yuhui Bao <julie.bao32@gmail.com>
2023-06-08 07:48:01 -0700

    add AllocatedPages col and let previously added cols have missing values instead of preset to empty str

---
+++ distributed/ddp-tutorial-series/nsys_singlegpu_multilayer/train_singlegpu_multilayer.py
@@ -0,0 +1,130 @@
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import torch.profiler
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision
+
+# import nvidia_dlprof_pytorch_nvtx
+
+# nvidia_dlprof_pytorch_nvtx.init()
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class Trainer:
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        train_data: DataLoader,
+        optimizer: torch.optim.Optimizer,
+        gpu_id: int,
+        save_every: int,
+    ) -> None:
+        self.gpu_id = gpu_id
+        self.model = model.to(gpu_id)
+        self.train_data = train_data
+        self.optimizer = optimizer
+        self.save_every = save_every
+
+    def _run_batch(self, source, targets):
+        self.optimizer.zero_grad()
+        output = self.model(source)
+        loss = F.cross_entropy(output, targets)
+        loss.backward()
+        self.optimizer.step()
+
+    def _run_epoch(self, epoch):
+        b_sz = len(next(iter(self.train_data))[0])
+        print(
+            f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}"
+        )
+
+        for step, data in enumerate(self.train_data):
+            if step >= 1:
+                break
+            source = data[0].to(self.gpu_id)
+            targets = data[1].to(self.gpu_id)
+            self._run_batch(source, targets)
+
+    def _save_checkpoint(self, epoch):
+        ckp = self.model.state_dict()
+        PATH = "checkpoint.pt"
+        torch.save(ckp, PATH)
+        print(f"Epoch {epoch} | Training checkpoint saved at {PATH}")
+
+    def train(self, max_epochs: int):
+        for epoch in range(max_epochs):
+            self._run_epoch(epoch)
+            # if epoch % self.save_every == 0:
+            #     self._save_checkpoint(epoch)
+
+
+def load_train_objs():
+    # train_set = MyTrainDataset(2048)  # load your dataset
+    # model = torch.nn.Linear(20, 1)  # load your model
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+
+    batch_size = 32
+
+    trainset = torchvision.datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transform
+    )
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, shuffle=True, num_workers=2
+    )
+    model = Net()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    return trainloader, model, optimizer
+
+
+def prepare_dataloader(dataset: Dataset, batch_size: int):
+    return DataLoader(dataset, batch_size=batch_size, pin_memory=True, shuffle=True)
+
+
+def main(device, total_epochs, save_every, batch_size):
+    train_data, model, optimizer = load_train_objs()
+    # train_data = prepare_dataloader(dataset, batch_size)
+    trainer = Trainer(model, train_data, optimizer, device, save_every)
+    trainer.train(total_epochs)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="simple distributed training job")
+    parser.add_argument(
+        "total_epochs", type=int, help="Total epochs to train the model"
+    )
+    parser.add_argument("save_every", type=int, help="How often to save a snapshot")
+    parser.add_argument(
+        "--batch_size",
+        default=32,
+        type=int,
+        help="Input batch size on each device (default: 32)",
+    )
+    args = parser.parse_args()
+
+    device = 0  # shorthand for cuda:0
+    main(device, args.total_epochs, args.save_every, args.batch_size)
